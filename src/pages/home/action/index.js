ac0fa8ee202d9d8bb004abc9add736a44e32fe1b
vladislav.b <nordob1408@mail.ru>
2019-02-28 15:32:50 +0300

    optimization code

---
+++ src/pages/home/action/index.js
@@ -0,0 +1,7 @@
+import 'blocks/product/product.sass'
+import 'blocks/new/index.sass'
+import 'blocks/actions/index.sass'
+
+export default $root => {
+
+};
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,9 +5,11 @@ import  initSets from './sets'
 import  initRolls from './rolls'
 import  initPizza from './pizza'
 import  initWok from './wok'
+import initAction from  './action'
+import initMotivation from  './motivation'
 import Swiper from "swiper/dist/js/swiper.min";
 import 'swiper/dist/css/swiper.min.css';
-
+import "tabslet/jquery.tabslet.min";
 
 export default $root => {
   let $intro = $root.find('.intro');
@@ -35,68 +37,33 @@ export default $root => {
     initWok($wok)
   }
 
-  new Swiper('.swiper__section', {
-    speed: 400,
-    slidesPerView: 4,
-    spaceBetween: 30,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  let $action = $root.find('.action');
+  if($action.length){
+    initAction($action)
+  }
 
-  });
+  let $motivation = $root.find('.motivation');
+  if($motivation.length){
+    initMotivation($motivation)
+  }
 
-  var input;
-  var price;
-  var startingPrice;
-
-
-  $root.find('.product__more').on('click', function () {
-    input = $(this).prev(".product__input").val();
-    price = $(this).parent('.product__quantity').prev('.product__price').text().replace(/[^0-9]/gi, '');
-    price = parseInt(price, 10);
-    if(input == 1) {
-      input++;
-      price = price - priceProduct;
-      price = input * price;
-      price = price + priceProduct;
-      price = price + " руб.";
-      $(this).prev('.product__input').val(input);
-      $(this).parent('.product__quantity').prev('.product__price').replaceWith('<div class="product__price">' + price + '</div>');
-    } else {
-      console.log(1);
-      price = price - priceProduct;
-      startingPrice = price/input;
-      input++;
-      price = startingPrice*input;
-      price = price + priceProduct;
-      price = price + " руб.";
-      $(this).prev('.product__input').val(input);
-      $(this).parent('.product__quantity').prev('.product__price').replaceWith('<div class="product__price">' + price + '</div>');
-    }
-  });
-
-  $root.find('.product__less').on('click', function () {
-    input = $(this).next(".product__input").val();
-    price = $(this).parent('.product__quantity').prev('.product__price').text().replace(/[^0-9]/gi, '');
-    price = parseInt(price, 10);
-    if(input == 1) {
-      console.log(1);
-    } else {
-      price = price - priceProduct;
-      startingPrice = price/input;
-      input--;
-      price = startingPrice*input;
-      price = price + priceProduct;
-      price = price + " руб.";
-      $(this).next('.product__input').val(input);
-      $(this).parent('.product__quantity').prev('.product__price').replaceWith('<div class="product__price">' + price + '</div>');
-    }
+  $root.find('.swiper__section').each((i, elem) => {
+    console.log(elem);
+    new Swiper(elem, {
+      speed: 400,
+      slidesPerView: 4,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        simulateTouch: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   });
 
   var products = {
@@ -114,64 +81,79 @@ export default $root => {
     }
   };
 
-  var nameProduct;
-  var priceProduct = 0;
-
   $.each(products,function(key,data) {
-    $root.find("select").append('<option class="product__sausesItem">' + data.name + '</option>');
+    $root.find("select").append('<option class="product__sausesItem" data-priceSause="'+data.price+'">' + data.name + '</option>');
+  });
+
+  class Counter {
+    constructor($root) {
+      this.$root = $root;
+      this.$input = $root.find('.product__input');
+      this.$price = $root.find('.product__price');
+      this.$sauce = $root.find('.product__pricetags span');
+      this.saucePrice = $root.find('select').find('option:selected').attr('data-pricesause');
+      this.$select = $root.find('select');
+      this.$countSupplements = $root.find('.product__howmuches span');
+      this.price = $root.data('price');
+      this.count = 1;
+      this.number = 0;
+      this.initButtons();
+    }
+
+    initButtons() {
+      this.$root.find('.product__more').on("click", () => {
+        this.count += 1;
+        this.$input.val(this.count);
+        this.$price.html(this.count * this.price + parseInt(this.number * this.saucePrice));
+      });
+
+      this.$root.find('.product__less').on("click", () => {
+        this.count = this.count > 1 ? this.count - 1 : 1;
+        this.$input.val(this.count);
+        this.$price.html(this.count * this.price + parseInt(this.number * this.saucePrice));
+      });
+
+      this.$root.find('.product__cancellation').on("click", () => {
+        this.$countSupplements.html(this.number = 0);
+        this.$sauce.html(0);
+        this.$price.html(this.count * this.price + parseInt(this.number * this.saucePrice));
+      });
+
+      this.$root.find('.product__add').on("click", () => {
+        this.number += 1;
+        this.$countSupplements.html(this.number);
+        this.$sauce.html(this.number * this.saucePrice);
+        this.$price.html(this.count * this.price + parseInt(this.number * this.saucePrice));
+      });
+
+      this.$root.find('select').on('change', () => {
+        this.saucePrice = this.$select.find('option:selected').attr('data-pricesause');
+        this.$countSupplements.html(this.number = 0);
+        this.$sauce.html(0);
+        this.$price.html(this.count * this.price + parseInt(this.number * this.saucePrice));
+      });
+    }
+  }
+
+  $('.product').each(function() {
+    new Counter($(this));
+  });
+
+
+  const $tabCaption  = $root.find(".action__caption");
+
+  $tabCaption.on('click', 'li:not(.active)', function () {
+
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      // .closest(".action__content").find(".action__tabs").removeClass('active').eq($(this).index()).addClass('active');
 
   });
 
-  var valhowmuch = 0;
-  var prevvalhowmuch = 0;
-
-  $root.find(".product__add").on('click', function (e) {
-    nameProduct = $(this).parent(".product__pricetag").parent(".product__settings").next(".product__sauces").children('select').val();
-    console.log( nameProduct);
-    $.each(products,function(key,data) {
-      if(data.name === nameProduct){
-        priceProduct = data.price;
-        console.log('priceProduct start', priceProduct);
-      }
-    });
-    prevvalhowmuch = priceProduct * valhowmuch;
-    valhowmuch++;
-    priceProduct *= valhowmuch;
-    console.log('priceProduct', priceProduct);
-    console.log('valhowmuch', valhowmuch);
-    $(this).prev(".product__pricetags").find('span').text(priceProduct);
-    $(this).parent(".product__pricetag").prev(".product__howmuch").children(".product__howmuches").replaceWith('<div class="product__howmuches">' + "+" + valhowmuch + '</div>');
-    console.log(price);
-    price = $(this).parent(".product__pricetag").parent(".product__settings").parent(".product__supplements").parent(".product__block").find(".product__price").text().replace(/[^0-9]/gi, '');
-    price = parseInt(price, 10);
-    console.log(price);
-    price = price - prevvalhowmuch;
-    price = price + priceProduct;
-    $(this).parent(".product__pricetag").parent(".product__settings").parent(".product__supplements").parent(".product__block").find(".product__price").replaceWith('<div class="product__price">' + price + " руб." + '</div>');
+  $('.action__content').tabslet({
+    mouseevent: 'click',
+    attribute: 'href',
+    animation: true
   });
-
-  $root.find("select").on('change', function (e) {
-    $(this).parent(".product__sauces").prev(".product__settings").find(".product__howmuches").replaceWith('<div class="product__howmuches">' + "+0" + '</div>');
-    $(this).parent(".product__sauces").prev(".product__settings").find('span').text("0");
-    price = $(this).parent(".product__sauces").parent(".product__supplements").parent(".product__block").find(".product__price").text().replace(/[^0-9]/gi, '');
-    price = parseInt(price, 10);
-    price = price - priceProduct;
-    $(this).parent(".product__sauces").parent(".product__supplements").parent(".product__block").find(".product__price").replaceWith('<div class="product__price">' + price + " руб." + '</div>');
-    priceProduct = 0;
-    valhowmuch = 0;
-
-  });
-
-  $root.find(".product__cancellation").on('click', function (e) {
-    $(this).prev(".product__howmuches").replaceWith('<div class="product__howmuches">' + "+0" + '</div>');
-    $(this).parent(".product__howmuch").next(".product__pricetag").find('span').text("0");
-    price = $(this).parent(".product__howmuch").parent(".product__settings").parent(".product__supplements").parent(".product__block").find(".product__price").text().replace(/[^0-9]/gi, '');
-    price = parseInt(price, 10);
-    price = price - priceProduct;
-    $(this).parent(".product__howmuch").parent(".product__settings").parent(".product__supplements").parent(".product__block").find(".product__price").replaceWith('<div class="product__price">' + price + " руб." + '</div>');
-    priceProduct = 0;
-    valhowmuch = 0;
-
-  })
 
 };
